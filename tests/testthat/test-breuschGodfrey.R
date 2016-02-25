bea5d4f95444fbd8748e1b2603f941c97c88cf47
Colin Olito <colin.olito@gmail.com>
2016-02-25 11:41:43 +1100

    Wrote tests for breuschGodfrey()

---
+++ tests/testthat/test-breuschGodfrey.R
@@ -0,0 +1,42 @@
+context("Breusch-Godfrey Statistic")
+
+# dependency for testing against behaviour of reference function
+library(lmtest)
+
+    x        <-  rnorm(100)
+    y        <-  rnorm(100, mean=2, sd=0.3) + rnorm(100, mean=3, sd=0.5) * x
+    x2        <-  rnorm(103)
+    y2       <-  rnorm(103, mean=2, sd=0.3) + rnorm(103, mean=3, sd=0.5) * x2
+    xNA     <-  x
+    yNA     <-  y
+    xNA[sample(c(1:100), 5)] <- NA
+    yNA[sample(c(1:100), 5)] <- NA
+    charVec  <-  letters[1:100]
+
+test_that("Simple corner cases", {
+    # returns correct behaviour regardless of input
+    expect_is(breuschGodfrey(y, x), "list")
+
+    # make sure output length matches expected behaviour
+    expect_identical(length(breuschGodfrey(y, x)), 3L)
+
+    # returns correct behaviour when lengths of x and y differ
+    expect_error(breuschGodfrey(y2, x), "incompatible dimensions")
+
+    # returns error if x is not numeric
+    expect_error(breuschGodfrey(y, charVec), "NA/NaN/Inf in 'x'")
+    expect_warning(breuschGodfrey(y, charVec), "NAs introduced by coercion")
+
+    # returns correct behavoiur when x, y have NAs
+    expect_error(breuschGodfrey(yNA, xNA), "NA/NaN/Inf in 'x'")
+})
+
+test_that("Computations are correct", {
+ 
+   # does calculated statistic match output from model function bgtest() from package lmtest?
+    expect_equivalent(breuschGodfrey(y, x)$bg, bgtest(y ~ x, order=(length(x)-3))$statistic)
+
+   # Check that bfN is being calculated as expected
+    expect_equivalent(breuschGodfrey(y, x)$bg / length(x), breuschGodfrey(y, x)$bgN)
+ 
+})
