ddabc83d2478b0bb4e14775052321fe9d95a4997
Colin Olito <colin.olito@gmail.com>
2016-02-29 12:35:35 +1100

    Fixed tests. re-built package. #4

--- tests/testthat/test-thinData.R
+++ tests/testthat/test-thinData.R
@@ -1,7 +1,6 @@
 context("Thinning data sets")
 
 data(thinned_cormorant_data)
-head(thinned_cormorant_data)
 thin2  <-  thinData(thinned_cormorant_data$Time,
 					thinned_cormorant_data$VO2.ml.min, by=2)
 thin3  <-  thinData(thinned_cormorant_data$Time,
@@ -13,62 +12,39 @@ len31  <-  length(seq(from=1, to=N, by=3))
 len32  <-  length(seq(from=2, to=N, by=3))
 len33  <-  length(seq(from=3, to=N, by=3))
 
-source('R/functions.R')
+x        <-  c(1:100)
+y        <-  sort(runif(100, min=8, max=50))
+y2       <-  y[1:90]
+yNA      <-  y
+yNA[sample(c(1:100), 5)] <- NA
+yNAtest  <-  yNA[!is.na(yNA)]
+thinNA   <- thinData(xall=x, yall=yNA, by=2)
+charVEC  <- sample(letters, 100, replace=TRUE)
 
 test_that("Simple corner cases", {
-
     # returns correct structure
     expect_is(thin2, "list")
     expect_identical(length(thin2), 2L)
     expect_identical(length(thin3), 3L)
-    expect_identical(nrow(thin2[[1]]), len21)
-    expect_identical(nrow(thin2[[2]]), len22)
-    expect_identical(nrow(thin3[[1]]), len31)
-    expect_identical(nrow(thin3[[2]]), len32)
-    expect_identical(nrow(thin3[[3]]), len33)
+    expect_identical(nrow(thin2$newData1), len21)
+    expect_identical(nrow(thin2$newData2), len22)
+    expect_identical(nrow(thin3$newData1), len31)
+    expect_identical(nrow(thin3$newData2), len32)
+    expect_identical(nrow(thin3$newData3), len33)
 
     # make sure it removes NAs from inputs
-    
+	expect_match(any(is.na(thin2$newData2)), "FALSE")
+	expect_match(any(is.na(thin3$newData2)), "FALSE")
+	expect_match(any(is.na(thin3$newData3)), "FALSE")
+	expect_identical(length(thinNA$newData2$y_thin),
+					 length(yNAtest[seq(from=2, to=length(yNAtest), by=2)]))
 
     # returns correct behaviour regardless of input
-    
+    expect_error(thinData(x=x, y=charVEC), "x must be numeric")
 
     # returns correct behaviour when lengths of x and y differ
- 	
+    expect_error(thinData(x=x, y=y2), "arguments imply differing number of rows: 100, 90")
 
 	# correct behaviour when "thin" is missing, defaults to 2
-    
-
-	# Returns data frame with equal spacing of observations
-
-#	expect_identical((nrow(thinned_cormorant_data) / nrow(newData)), 3)
-
-
-    # returns correct structure
-#    expect_is(allRegs1, "rankLocReg")
-#    expect_is(allRegs2, "rankLocReg")
-#    expect_is(allRegs3, "rankLocReg")
-#    expect_identical(length(allRegs1), 6L)
-#    expect_identical(length(allRegs2), 6L)
-#    expect_identical(length(allRegs3), 6L)
-#
-#    # make sure it removes NAs from inputs
-#    expect_identical(length(allRegs3$xall), length(xNA[!is.na(xNA) & !is.na(yNA)]))
-#
-#    # returns correct behaviour regardless of input
-#    expect_error(rankLocReg(xall=charVec, yall=y, alpha=0.8, method='eq'), 'x must be numeric')
-#    expect_error(rankLocReg(xall=x, yall=charVec, alpha=0.8, method='eq'), 'x must be numeric')
-#
-#    # returns correct behaviour when lengths of x and y differ
-#    expect_error(rankLocReg(xall=x, yall=y[-c(10)], alpha=0.8, method='eq'))
-# 	
-#  	 # throws error when missing alpha
-#    expect_error(rankLocReg(xall=x[order(x)], yall=y[order(x)]), 'argument "alpha" is missing, with no default')
-#
-#    # correct behaviour when method is missing, defaults to 'ns'
-#    expect_is(rankLocReg(xall=x[order(x)], yall=y[order(x)], alpha=0.8), "rankLocReg")
-#    expect_identical(rankLocReg(xall=x[order(x)], yall=y[order(x)], alpha=0.8)$method, "ns")
-#
-#    # throws warning when x is unordered
-#    expect_warning(rankLocReg(xall=x, yall=y, alpha=0.8, method='eq'), "Dataset must be ordered by xall")
+	expect_identical(length(thinData(x=x, y=y)), 2L)
 })
